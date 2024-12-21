21f4fff0782ad603252a65a1607e041991db7879
dillionlim <dillionlim2004@gmail.com>
2024-12-21 14:50:20 +0800

    feat: add day 20 missing solutions

--- src/day_20/day_20a/20a.rs
+++ src/day_20/day_20a/20a.rs
@@ -1,13 +1,87 @@
 // Implementation for Day 20, Part A
 use crate::common::{get_input, process_input};
 use std::path::Path;
+use hashbrown::HashSet;
+
+const DIRS: &[(isize, isize)] = &[(0, 1), (1, 0), (0, -1), (-1, 0)];
+
+fn in_bounds(x: isize, y: isize, x_len: usize, y_len: usize) -> bool {
+    x >= 0 && x < x_len as isize && y >= 0 && y < y_len as isize
+}
 
 pub fn solve() -> String {
     let source_file = file!();
     let dir_path = Path::new(&source_file).parent().unwrap().to_path_buf();
     let input = get_input(dir_path);
-    let processed_input: Vec<Vec<char>> = process_input(input, "char");
+    let grid: Vec<Vec<char>> = process_input(input, "char");
+
+    let start = grid
+    .iter()
+    .enumerate()
+    .filter_map(|(i, row)| {
+        row.iter()
+            .enumerate()
+            .find_map(|(j, &v)| if v == 'S' { Some((i as isize, j as isize)) } else { None })
+    })
+    .next()
+    .unwrap();
+
+    let mut vis = vec![false; grid.len() * grid[0].len()];
+    let mut path = Vec::new();
+    let mut on_path = vec![0usize; grid.len() * grid[0].len()];
+    let mut currx = start.0 as isize;
+    let mut curry = start.1 as isize;
+    let mut ind = 0;
+
+    vis[currx as usize * grid[0].len() + curry as usize] = true;
+
+    loop {
+        path.push((ind, currx, curry));
+        on_path[currx as usize * grid[0].len() + curry as usize] = ind;
+        if grid[currx as usize][curry as usize] == 'E' {
+            break;
+        }
+        for (dx, dy) in DIRS {
+            let newx = currx as isize + dx;
+            let newy = curry as isize + dy;
+            if (grid[newx as usize][newy as usize] == '.' ||
+            grid[newx as usize][newy as usize] == 'E')
+            && !vis[newx as usize * grid[0].len() + newy as usize] {
+                vis[newx as usize * grid[0].len() + newy as usize] = true;
+                currx = newx;
+                curry = newy;
+                break;
+            }
+        }
+        ind += 1;
+    }
     
+    const MAXDIST: isize = 2;
+    let mut ans = HashSet::new();
+
+    for (i, x, y) in path.iter() {
+        for dx in -MAXDIST..=MAXDIST {
+            let max_dy = MAXDIST - dx.abs();
+            for dy in -max_dy..=max_dy {
+                let newx = x + dx;
+                let newy = y + dy;
+                let dist = newx.abs_diff(*x) + newy.abs_diff(*y);
+
+                if in_bounds(newx, newy, grid.len(), grid[0].len()) 
+                && on_path[newx as usize * grid[0].len() + newy as usize] != 0
+                && on_path[newx as usize * grid[0].len() + newy as usize].abs_diff(*i) >= dist + 100 {
+                    let new_hash = newx as usize * grid[0].len() + newy as usize;
+                    let old_hash = *x as usize * grid[0].len() + *y as usize;
+                    if new_hash > old_hash {
+                        ans.insert((new_hash, old_hash));
+                    }
+                    else {
+                        ans.insert((old_hash, new_hash));
+                    }
+                }
+            }
+        }    
+    }
     
-    return input.join("\n");
+    return ans.len().to_string();
 }
--- src/day_20/day_20b/20b.rs
+++ src/day_20/day_20b/20b.rs
@@ -1,12 +1,63 @@
 // Implementation for Day 20, Part B
 use crate::common::{get_input, process_input};
 use std::path::Path;
+use itertools::Itertools;
+
+const DIRS: &[(isize, isize)] = &[(0, 1), (1, 0), (0, -1), (-1, 0)];
 
 pub fn solve() -> String {
     let source_file = file!();
     let dir_path = Path::new(&source_file).parent().unwrap().to_path_buf();
     let input = get_input(dir_path);
-    // let processed_input: Vec<Vec<i32>> = process_input(input, "i32");
+    let grid: Vec<Vec<char>> = process_input(input, "char");
+
+    let start = grid
+    .iter()
+    .enumerate()
+    .filter_map(|(i, row)| {
+        row.iter()
+            .enumerate()
+            .find_map(|(j, &v)| if v == 'S' { Some((i as isize, j as isize)) } else { None })
+    })
+    .next()
+    .unwrap();
+
+    let mut vis = vec![false; grid.len() * grid[0].len()];
+    let mut path = Vec::new();
+    let mut currx = start.0 as isize;
+    let mut curry = start.1 as isize;
+    let mut ind = 0usize;
+
+    vis[currx as usize * grid[0].len() + curry as usize] = true;
+
+    loop {
+        path.push((ind, currx, curry));
+        if grid[currx as usize][curry as usize] == 'E' {
+            break;
+        }
+        for (dx, dy) in DIRS {
+            let newx = currx as isize + dx;
+            let newy = curry as isize + dy;
+            if (grid[newx as usize][newy as usize] == '.' ||
+            grid[newx as usize][newy as usize] == 'E')
+            && !vis[newx as usize * grid[0].len() + newy as usize] {
+                vis[newx as usize * grid[0].len() + newy as usize] = true;
+                currx = newx;
+                curry = newy;
+                break;
+            }
+        }
+        ind += 1;
+    }
+
+    let mut ans = 0;
+
+    for ((i1, x1, y1), (i2, x2, y2)) in path.iter().tuple_combinations() {
+        let d = x1.abs_diff(*x2) + y1.abs_diff(*y2);
+        if d <= 20 && i1.abs_diff(*i2) >= d + 100 {
+            ans += 1;
+        }
+    }
     
-    return input.join("\n");
+    return ans.to_string();
 }
